e1952a13b33eca5a8611348b2043ee4bb2cdb185
Atenas <naylinatenas@gmail.com>
2023-05-01 23:14:38 -0500

    Añadir la funcionalidad de desencriptar y corregir algunos errores de encriptación

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function encriptar() {
+
   let texto = document.getElementById("texto").value;
   let tituloMensaje = document.getElementById("titulo-mensaje");
   let parrafo = document.getElementById("parrafo");
@@ -23,4 +24,27 @@ function encriptar() {
         parrafo.textContent="Ingresa el texto que deseas encriptar o desencriptar";
         alert("Ingresa algún texto");
       }
+}
+
+function desencriptar() {
+  let texto=document.getElementById("texto").value;
+
+  let textoCifrado=texto
+  .replace(/enter/gi, "e")
+  .replace(/ines/gi, "i")
+  .replace(/ai/gi, "a")
+  .replace(/ober/gi, "o")
+  .replace(/ufat/gi, "u");
+
+  if (texto.length != 0) {
+    document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto desencriptado con éxito";
+    parrafo.textContent = "";
+    muñeco.src = "./img/desencriptado.jpg";
+  } else {
+    muñeco.src = "./img/muñeco.png";
+    tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    swal("Ooops!", "Debes ingresar un texto", "warning");
+  }
 }
